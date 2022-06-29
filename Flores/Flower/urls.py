c41ab113e4576534fca0f0fb98be71f1f09fc81f
Brimanda <br.miranda@duocuc.cl>
2022-06-29 10:57:03 -0400

    Avance

--- Flores/Flower/urls.py
+++ Flores/Flower/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import home, productos, quienessomos, fundacion, registro, aproducto, lproducto, mproducto, FlorViewSet, eproducto, enciclopedia
 from rest_framework import routers
+from django.contrib.auth.decorators import login_required
 
 router = routers.DefaultRouter()
 router.register('flores', FlorViewSet)
@@ -11,10 +12,10 @@ urlpatterns = [
     path('productos/', productos, name="productos"),
     path('fundacion/', fundacion, name="fundacion"),
     path('registro/', registro, name="registro"),
-    path('aproducto/', aproducto, name="aproducto"),
-    path('lproducto/', lproducto, name="lproducto"),
-    path('mproducto/<id>/', mproducto, name="mproducto"),
+    path('aproducto/', login_required(aproducto), name="aproducto"),
+    path('lproducto/', login_required(lproducto), name="lproducto"),
+    path('mproducto/<id>/', login_required(mproducto), name="mproducto"),
     path('api/', include(router.urls)),
-    path('eproducto/<id>/', eproducto, name="eproducto"),
+    path('eproducto/<id>/', login_required(eproducto), name="eproducto"),
     path('enciclopedia/', enciclopedia, name="enciclopedia")
 ]
